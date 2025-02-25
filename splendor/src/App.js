931445c7047602f21f3435f39a953ee1c98a73c4
23clhelle <chellebusch2@unl.edu>
2025-02-25 14:44:42 -0600

    Add Background to app

--- splendor/src/App.js
+++ splendor/src/App.js
@@ -1,7 +1,6 @@
-
 import Home from "./pages/Home.js"
 import firebase from "./firebase.js"
-
+import "./App.css"
 
 
 function MyButton() {
@@ -13,13 +12,17 @@ function MyButton() {
   }
   
   export default function MyApp() {
+    
     return (
       <>
         <div className="App">
           <Home/>
-        </div>
-        <div className='first-button'>
-          <MyButton/>
+          <div className='first-button'>
+            <MyButton/>
+          </div>
+          <div className="GemCard">
+            <img src="/Images/MainCards/Blue 1.0.png" alt="Card" />
+          </div>
         </div>
       </>
     );
