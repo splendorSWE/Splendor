02e40f6076b54fa1f657f6bdc662068acaeb7a8a
23clhelle <chellebusch2@unl.edu>
2025-02-26 12:20:16 -0600

    fix index.js error

--- splendor/src/index.js
+++ splendor/src/index.js
@@ -19,11 +19,6 @@ root.render(
   </React.StrictMode>
 );
 
-ReactDOM.render(
-  
-  document.getElementById('root')
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
