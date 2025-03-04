0a81bdc77b94d6e89169a7b1b4909caa672350fa
Zohaib2421 <zohaibshaikh2421@gmail.com>
2025-03-04 14:00:37 -0600

    Created baseline for home page.

--- splendor/src/pages/Home.jsx
+++ splendor/src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css"; 
 
 export default function Home() {
-    return(
-        <div>home</div>
-    )
+  return (
+    <div className="home-page">
+      <header className="home-header">
+        <img src="/Images/splendor-logo.png" alt="Splendor Logo" className="logo" />
+      </header>
+      <main className="home-menu">
+        <Link to="/gameboard">
+          <button className="menu-button">Gameboard</button>
+        </Link>
+        <Link to="/rules">
+          <button className="menu-button">Rules</button>
+        </Link>
+        <Link to="/create-game">
+          <button className="menu-button">Create Game</button>
+        </Link>
+        <Link to="/join-game">
+          <button className="menu-button">Join Game</button>
+        </Link>
+        <Link to="/profile">
+          <button className="menu-button">Profile</button>
+        </Link>
+      </main>
+    </div>
+  );
 }
