27c5ac65ae7821f035ce0df95bdeabf86a2a5a52
Zohaib2421 <zohaibshaikh2421@gmail.com>
2025-03-11 14:17:23 -0500

    Replace "Sign in" button with "Rules"

--- splendor/src/components/PageHeader.jsx
+++ splendor/src/components/PageHeader.jsx
@@ -12,7 +12,7 @@ export default function PageHeader({ title }) {
         <div className="spacer"></div> {/* Flexible space to push title to center */}
         <div className="nav-buttons">
           <NavigationButton destination="Home" link="/" styling='dark-button' />
-          <NavigationButton destination="Sign In" link="/user-auth" styling='dark-button' />
+          <NavigationButton destination="Rules" link="/user-auth" styling='dark-button' />
         </div>
       </div>
     );
