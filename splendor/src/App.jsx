f675794f25bd607ed12066dbbeb3b53110b11210
Zohaib2421 <zohaibshaikh2421@gmail.com>
2025-03-19 16:03:30 -0500

    Changed the home screen code to be in Home.jsx. Commented out the cod for WebSockets in the meantime while they are not in use.

--- splendor/src/App.jsx
+++ splendor/src/App.jsx
@@ -3,12 +3,10 @@ import Home from "./pages/Home"
 import "./App.css"
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import UserStats from "./components/UserStats";
-import PageHeader from "./components/PageHeader";
-import NavigationButton from "./components/NavigationButton";
+
 
 // Connect to WebSocket server
-const socket = io("http://localhost:4000");
+// const socket = io("http://localhost:4000");
 
 
 
@@ -16,59 +14,24 @@ const socket = io("http://localhost:4000");
 
 
 export default function MyApp() {
-  const [messages, setMessages] = useState([]);
-  const [showUserStats, setShowUserStats] = useState(false);
-  const [profilePic, setProfilePic] = useState("../images/default_pfp.jpg")
 
-  // Listen for messages from the server
-  useEffect(() => {
-    socket.on("message", (msg) => {
-      console.log("Message from server:", msg);
-      setMessages((prev) => [...prev, msg]);
-    });
+  // // Listen for messages from the server
+  // useEffect(() => {
+  //   socket.on("message", (msg) => {
+  //     console.log("Message from server:", msg);
+  //     setMessages((prev) => [...prev, msg]);
+  //   });
 
   
-    // Clean up listener on component unmount
-    return () => socket.off("message");
-  }, []);
+  //   // Clean up listener on component unmount
+  //   return () => socket.off("message");
+  // }, []);
 
-  const toggleUserStats = () => {
-    setShowUserStats((prev) => !prev);
-  };
+  
   return (
     
       <div className="App">
         <Home/>
-        
-        <div className="page-header"><PageHeader title='Home'/></div>
-          
-        <div className="content-container">
-          <div>
-            {/* <Home/> */}
-            <NavigationButton styling='dark-button vertical'
-              destination="Gameboard"
-              link='/gameboard'/>
-            <NavigationButton 
-            styling='dark-button vertical'
-            destination="Rules"
-            link='/rules'/>
-            <NavigationButton 
-            styling='dark-button vertical'
-            destination="User Profile"
-            link='/profile'
-            state={{ profilePic}}/>
-            
-            
-          </div>
-          <div>
-            <button className='dark-button'
-            onClick={toggleUserStats}>
-              {showUserStats ? "Hide User Stats" : "Show User Stats"}
-              {showUserStats && <UserStats />}
-            </button>
-          </div>
-
-        </div>
       </div>
       
   );
--- splendor/src/pages/Home.jsx
+++ splendor/src/pages/Home.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import UserStats from "./../components/UserStats";
+import PageHeader from "./../components/PageHeader";
+import NavigationButton from "./../components/NavigationButton";
+
 
 export default function Home() {
+    const [messages, setMessages] = useState([]);
+    const [showUserStats, setShowUserStats] = useState(false);
+    const [profilePic, setProfilePic] = useState("../images/default_pfp.jpg")
+    const toggleUserStats = () => {
+        setShowUserStats((prev) => !prev);
+    };
+
     return(
-        <div>home</div>
+        <div>
+            <div className="page-header"><PageHeader title='Home'/></div>
+            
+            <div className="content-container">
+            <div>
+                {/* <Home/> */}
+                <NavigationButton styling='dark-button vertical'
+                destination="Gameboard"
+                link='/gameboard'/>
+                <NavigationButton 
+                styling='dark-button vertical'
+                destination="Rules"
+                link='/rules'/>
+                <NavigationButton 
+                styling='dark-button vertical'
+                destination="User Profile"
+                link='/profile'
+                state={{ profilePic}}/>
+                
+                
+            </div>
+            <div>
+                <button className='dark-button'
+                onClick={toggleUserStats}>
+                {showUserStats ? "Hide User Stats" : "Show User Stats"}
+                {showUserStats && <UserStats />}
+                </button>
+            </div>
+
+            </div>
+        </div>
     )
 }
