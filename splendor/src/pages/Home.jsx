95eddd11605b687869cc95749e4dacab3f45bcfe
23clhelle <chellebusch2@unl.edu>
2025-04-01 15:24:32 -0500

    left box done

--- splendor/src/pages/Home.jsx
+++ splendor/src/pages/Home.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+import './pageStyles/Home.css'
 import PageHeader from "../components/PageHeader";
 import NavigationButton from "../components/NavigationButton";
+import { Link } from 'react-router-dom';
+{/* 
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import UserStats from "../components/UserStats";
-import './pageStyles/Home.css'
 
 const socket = io("http://localhost:4000");
+*/}
+
+function GameChoiceButton({title}) {
+    return(
+      <button class='game-choice-button' title={title}>
+          {title}
+      </button>
+    )
+  }
+
+  const buttons = document.querySelectorAll('.game-choice-button');
+  let selectedButton = null;
+
+  buttons.forEach(button => {
+      button.addEventListener('click', () => {
+          if (selectedButton) {
+              selectedButton.classList.remove('selected');
+          }
+          button.classList.add('selected');
+          selectedButton = button;
+          // Do something with the selected value
+          console.log('Selected value:', button.dataset.value);
+      });
+  });
 
 export default function Home() {
 
+    {/* 
     // Listen for messages from the server
     useEffect(() => {
         socket.on("message", (msg) => {
@@ -20,28 +47,47 @@ export default function Home() {
         // Clean up listener on component unmount
         return () => socket.off("message");
     }, []);
-
+    
     const toggleUserStats = () => {
         setShowUserStats((prev) => !prev);
       };
       const [messages, setMessages] = useState([]);
         const [showUserStats, setShowUserStats] = useState(false);
         const [profilePic, setProfilePic] = useState("../images/default_pfp.jpg")
+
+        */}
     
     return(
-        <div className="Home">
+        
+        <div>
             <div className="page-header"><PageHeader title='Home'/></div>
             
-            <div className="content-container">
-                
+            <div className="home-container">
                 <div className='option-box'>
-
+                    <div className='create-game-box'>
+                        <h1>
+                            Create Game
+                        </h1>
+                        <div className='button-row'>
+                            <GameChoiceButton title={"Public"}/>
+                            <GameChoiceButton title={"Private"}/>
+                        </div>
+                        
+                        <p className='turn-timer'>
+                            Turn Timer:
+                        </p>
+                        <Link to="/Gameboard">
+                            <button class='create-button'>
+                                Create Game
+                            </button>
+                        </Link>
+                    </div>
                 </div>
                 <div className='option-box'>
-                    
+                    Hi
                 </div>
-                
             </div>
+            
         </div>
     )
 }
