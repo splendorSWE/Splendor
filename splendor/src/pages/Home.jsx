0f80d70fe4c3ee4df29cc02a4ba4bde70d99c6e9
23clhelle <chellebusch2@unl.edu>
2025-04-08 15:08:46 -0500

    fixed selection stuff

--- splendor/src/pages/Home.jsx
+++ splendor/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './pageStyles/Home.css'
 import PageHeader from "../components/PageHeader";
@@ -12,32 +12,31 @@ import UserStats from "../components/UserStats";
 const socket = io("http://localhost:4000");
 */}
 
-function GameChoiceButton({title}) {
-    return(
-      <button class='game-choice-button' title={title}>
-          {title}
-      </button>
-    )
-  }
+function GameChoiceButton({ title, selected, onClick }) {
+    return (
+        <button
+            className={`game-choice-button ${selected ? "selected" : ""}`}
+            onClick={onClick}
+        >
+            {title}
+        </button>
+    );
+}
 
-  const buttons = document.querySelectorAll('.game-choice-button');
   let selectedButton = null;
 
-  buttons.forEach(button => {
-      button.addEventListener('click', () => {
-          if (selectedButton) {
-              selectedButton.classList.remove('selected');
-          }
-          button.classList.add('selected');
-          selectedButton = button;
-          // Do something with the selected value
-          console.log('Selected value:', button.dataset.value);
-      });
-  });
 
 export default function Home() {
 
-    {/* 
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelectedOption(option);
+        console.log('Selected option:', option);
+    };
+    
+    {
+        /* 
     // Listen for messages from the server
     useEffect(() => {
         socket.on("message", (msg) => {
@@ -57,6 +56,8 @@ export default function Home() {
         const [profilePic, setProfilePic] = useState("../images/default_pfp.jpg")
 
         */}
+
+    
     
     return(
         
@@ -68,25 +69,33 @@ export default function Home() {
                         Create Game
                     </h1>
                     <div className='button-row'>
-                        <GameChoiceButton title={"Public"}/>
-                        <GameChoiceButton title={"Private"}/>
+                        <GameChoiceButton
+                            title="Public"
+                            selected={selectedOption === "Public"}
+                            onClick={() => handleSelect("Public")}
+                        />
+                        <GameChoiceButton
+                            title="Private"
+                            selected={selectedOption === "Private"}
+                            onClick={() => handleSelect("Private")}
+                        />
                     </div>
                     <p className='turn-timer'>
                         Turn Timer:
                     </p>
                     <Link to="/Gameboard">
-                        <button class='create-button'>
+                        <button className='create-button'>
                             Create Game
                         </button>
-                        </Link>
+                    </Link>
                 </div>
                 <div className='option-box'>
                     <h1 className='box-header'>
                         Join Game
                     </h1>
-                    <input class='game-code-input-box'type="number" placeholder="Enter code here"></input>
+                    <input className='game-code-input-box'type="number" placeholder="Enter code here"></input>
                     <Link to="/Gameboard">
-                        <button class='create-button'>
+                        <button className='create-button'>
                             Join Game
                         </button>
                     </Link>
