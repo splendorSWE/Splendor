f59c5075ac83ebb4d33dfa9b860e41a39603d612
23clhelle <chellebusch2@unl.edu>
2025-05-06 17:16:22 -0500

    PlayerCollection CSS

--- splendor/src/components/GameboardComponents/PlayerCollection.jsx
+++ splendor/src/components/GameboardComponents/PlayerCollection.jsx
@@ -1,6 +1,7 @@
 import ReservedCard from './ReservedCard';
 import CollectionCard from './CollectionCard';
 import Token from './Token';
+import '../componentStyles/PlayerCollection.css';
 
 export default function PlayerCollection({ Points, tokens, playerCards, viewCard, setViewCard, reservable, reservedCard, setSelectedCard, selectedCard }) {
   
