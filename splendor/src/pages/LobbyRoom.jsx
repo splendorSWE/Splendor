e1f01de5f521f13fd6309e76512a31763340a5f6
23clhelle <chellebusch2@unl.edu>
2025-05-06 12:50:57 -0500

    some lobby ui

--- splendor/src/pages/LobbyRoom.jsx
+++ splendor/src/pages/LobbyRoom.jsx
@@ -119,18 +119,18 @@ export default function LobbyRoom() {
 
       <div className="main-container">
         <div className="main-box">
+        <p className="lobby-username">Lobby Code: <strong>{lobbyCode}</strong></p>
         <p className="lobby-username">
             You are: <strong>{displayName}</strong>
           </p>
           <h2>Players in Lobby</h2>
-          
-
+    
           <ul>
             {players.map((p, i) => (
               <li key={i}>{p}</li>
             ))}
           </ul>
-          <p className="lobby-username">Lobby Code: <strong>{lobbyCode}</strong></p>
+
           <button
             className={`ready-button ${isReady ? "ready" : "not-ready"}`}
 
@@ -152,7 +152,7 @@ export default function LobbyRoom() {
             {isReady ? "Unready" : "Ready Up"}
           </button>
           <button
-            className="create-button"
+            className="ready-button"
             onClick={() => {
               socket.emit("leave_lobby");
 
